d3fc83d32261ab7284ea3002390985de051b4c32
Ihsane Fassih <fassih.ihsane@gmail.com>
2020-06-02 15:52:49 +0100

    Update

---
+++ app/src/main/java/com/example/protrip/FirstLunchActivity.java
@@ -0,0 +1,14 @@
+package com.example.protrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstLunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_lunch);
+    }
+}
