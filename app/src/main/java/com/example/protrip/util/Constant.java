5d5c5fd75a03ec91fc74b481253c5da8cb5bce2b
Yasser.Akbbach <yasser10873@gmail.com>
2020-06-16 03:27:47 +0100

    Filter trips by date (only trips of today) Added MyTripsActivity (FirebaseRecyclerView) Added TripsViewHolder

--- app/src/main/java/com/example/protrip/util/Constant.java
+++ app/src/main/java/com/example/protrip/util/Constant.java
@@ -3,6 +3,7 @@ package com.example.protrip.util;
 public interface Constant {
 
     int SPLASH_DELAY = 3000;
+    String DATE_FORMAT = "y-MM-d k:m";
 
     // Firebase documents
     String USERS = "Users";
